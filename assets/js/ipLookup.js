cd7988d14464da15254451e0f9c243d9257e2f4b
Adeem <adeemmawani@gmail.com>
2021-09-24 11:41:27 -0400

    Updated IP Lookup

--- assets/js/ipLookup.js
+++ assets/js/ipLookup.js
@@ -9,75 +9,106 @@ function lookupIP(ip) {
 
   else {
     toggleLoad(true);
-      
+
     $('#ipOutput').html("");
 
-    var serviceList = [
+    var categoryList = [
       {
-        "title": "AbuseIPDB - Blacklist",
-        "url": "https://www.abuseipdb.com/check/"
+        "category": "Blacklist",
+        "services": [
+          {
+            "title": "AbuseIPDB",
+            "url": "https://www.abuseipdb.com/check/"
+          },
+          {
+            "title": "ScamAlytics",
+            "url": "https://scamalytics.com/ip/"
+          },
+          {
+            "title": "StopForumSpam",
+            "url": "https://www.stopforumspam.com/ipcheck/"
+          }
+        ]
       },
       {
-        "title": "Censys - Scanner",
-        "url": "https://censys.io/ipv4/"
+        "category": "DNS",
+        "services": [
+          {
+            "title": "Google - Reverse DNS",
+            "url": "https://dns.google.com/query?name="
+          },
+          {
+            "title": "SecurityTrails - Passive DNS",
+            "url": "https://securitytrails.com/list/ip/"
+          }
+        ]
       },
       {
-        "title": "Cisco Talos - Threat Intelligence",
-        "url": "https://talosintelligence.com/reputation_center/lookup?search="
+        "category": "Scanner",
+        "services": [
+          {
+            "title": "Censys",
+            "url": "https://censys.io/ipv4/"
+          },
+          {
+            "title": "Shodan",
+            "url": "https://www.shodan.io/host/"
+          }
+        ]
       },
       {
-        "title": "Google - Reverse DNS",
-        "url": "https://dns.google.com/query?name="
+        "category": "Threat Intelligence",
+        "services": [
+          {
+            "title": "Cisco Talos",
+            "url": "https://talosintelligence.com/reputation_center/lookup?search="
+          },
+          {
+            "title": "GreyNoise",
+            "url": "https://www.greynoise.io/viz/ip/"
+          },
+          {
+            "title": "Open Threat Exchange",
+            "url": "https://otx.alienvault.com/indicator/ip/"
+          },
+          {
+            "title": "ThreatFox",
+            "url": "https://threatfox.abuse.ch/browse.php?search=ioc%3A"
+          },
+          {
+            "title": "ThreatMiner",
+            "url": "https://www.threatminer.org/host.php?q="
+          },
+          {
+            "title": "VirusTotal",
+            "url": "https://www.virustotal.com/gui/ip-address/"
+          }
+        ]
       },
       {
-        "title": "GreyNoise - Threat Intelligence",
-        "url": "https://www.greynoise.io/viz/riot/"
-      },
-      {
-        "title": "IPInfo - Generic Info",
-        "url": "https://ipinfo.io/"
-      },
-      {
-        "title": "IPLocation - GeoIP",
-        "url": "https://www.iplocation.net/ip-lookup?query="
-      },
-      {
-        "title": "Open Threat Exchange - Threat Intelligence",
-        "url": "https://otx.alienvault.com/indicator/ip/"
-      },
-      {
-        "title": "ScamAlytics - Blacklist",
-        "url": "https://scamalytics.com/ip/"
-      },
-      {
-        "title": "SecurityTrails - Passive DNS",
-        "url": "https://securitytrails.com/list/ip/"
-      },
-      {
-        "title": "Shodan - Scanner",
-        "url": "https://www.shodan.io/host/"
-      },
-      {
-        "title": "ThreatFox - Threat Intelligence",
-        "url": "https://threatfox.abuse.ch/browse.php?search=ioc%3A"
-      },
-      {
-        "title": "ThreatMiner - Threat Intelligence",
-        "url": "https://www.threatminer.org/host.php?q="
-      },
-      {
-        "title": "VirusTotal - Threat Intelligence",
-        "url": "https://www.virustotal.com/gui/ip-address/"
+        "category": "Other",
+        "services": [
+          {
+            "title": "IPInfo - Generic Info",
+            "url": "https://ipinfo.io/"
+          },
+          {
+            "title": "IPLocation - GeoIP",
+            "url": "https://www.iplocation.net/ip-lookup?query="
+          }
+        ]
       }
     ]
 
     $('#ipBar').css("display", "block");
 
-    $('#ipOutput').append("<ul>");
-    for (const service of serviceList) {
-      $('#ipOutput').append(`<li><a href="${service.url}${ip}" target="_blank">${service.title}</a></li>`);
+    for (const category of categoryList) {
+      $('#ipOutput').append(`<h3 style="margin: 0 0 0.25rem 0">${category.category}</h3>`);
+
+      $('#ipOutput').append($("<ul>").append(category.services.map(service =>
+        $("<li>").append(`<a href="${service.url}${ip}" target="_blank">${service.title}</a>`)
+      )));
     }
-    $('#ipOutput').append("</ul>");
 
     toggleLoad(false);
   }
